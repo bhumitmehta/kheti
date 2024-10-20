767f34aabf3c1d1e44437722a173391bebf140e4
bhumitmehta <bhumitmehta2209@gmail.com>
2024-10-20 23:52:23 +0530

    thie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Equipments from "../components/homeComponent/equipments/Equipments";
 import Banner from "../components/homeComponent/banner/Banner";
 import Services from "../components/homeComponent/services/Services";
 import Stats from "../components/homeComponent/stats/Stats";
-import Workflow from "../components/homeComponent/workflow/Workflow";
+
 import Support from "../components/homeComponent/support/Support";
 // import Faqs from '../components/homeComponent/faqs/Faqs'
 // import ContactUs from '../components/homeComponent/contactUs/ContactUs'
